c179179085c599a9524a7a20a970f7aafaaf6b88
Cristhian Cantero <cristhian.cantero@init-dev.com.ar>
2022-09-28 18:53:26 -0300

    Consultas GET con Axios para modelos basicos

--- src/components/pages/Talleres.js
+++ src/components/pages/Talleres.js
@@ -1,6 +1,38 @@
+import axios from 'axios';
 import React from 'react';
-import '../../App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function Talleres() {
-  return <h1 className='talleres'>TALLERES</h1>;
+  const [listadoTalleres, setlistadoTalleres] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/talleres`)
+      .then(res => setlistadoTalleres(res.data.data))
+      .catch(err => console.log(`Error: ${err}`));
+  }, []);
+
+  const verTalleres_HTMLTABLE = listadoTalleres.map((item) => {
+    return (
+      <tr key={item.id_taller}>
+        <td>{item.id_taller}</td>
+        <td>{item.nombre}</td>
+        <td>{item.direccion}</td>
+        <td>
+          <button type='button' className='btn btn-primary'>Actualizar</button>
+        </td>
+        <td>
+          <button type='button' className='btn btn-danger'>Desactivar</button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <>
+      <div className="d-flex justify-content-center p-4">
+        <h1 className='name'>Administracion de Repuestos</h1>
+      </div>
+    </>
+  );
 }
--- src/components/pages/Vehiculos.js
+++ src/components/pages/Vehiculos.js
@@ -1,6 +1,66 @@
+import axios from 'axios';
 import React from 'react';
-import '../../App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function Vehiculos() {
-  return <h1 className='vehiculos'>VEHICULOS</h1>;
+  const [listadoVehiculos, setlistadoVehiculos] = useState([]);
+  const [listadoMarcas, setlistadoMarcas] = useState([]);
+
+
+  useEffect(() => {
+    axios.get(`/api/vehiculos`)
+      .then(res => setlistadoVehiculos(res.data.data))
+      .catch(err => console.log(`Error: ${err}`));
+  }, []);
+
+  useEffect(() => {
+    axios.get(`/api/marcas`)
+      .then(res => setlistadoMarcas(res.data.data))
+      .catch(err => console.log(`Error: ${err}`));
+  }, []);
+
+  console.log(listadoMarcas)
+
+  const verVehiculos_HTMLTABLE = listadoVehiculos.map((item) => {
+    var indexMarca = listadoMarcas.findIndex((marca) => marca.id_marca == item.id_marca);
+    const objMarcaVehiculo = listadoMarcas[indexMarca];
+    // console.log(objMarcaVehiculo)
+    return (
+      <tr key={item.id_vehiculo}>
+        <td>{item.id_vehiculo}</td>
+        <td>{item.nombre}</td>
+        <td>{item.id_marca}</td>
+        <td>
+          <button type='button' className='btn btn-primary'>Actualizar</button>
+        </td>
+        <td>
+          <button type='button' className='btn btn-danger'>Desactivar</button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <>
+      <div className="d-flex justify-content-center p-4">
+        <h1 className='name'>Administracion de Repuestos</h1>
+      </div>
+      <div className="d-flex justify-content-center">
+        <table className="marcaTable table table-hover">
+          <thead className="table-dark">
+            <tr>
+              <th>ID Vehiculo</th>
+              <th>Nombre</th>
+              <th>Marca</th>
+              <th colSpan={2}>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {verVehiculos_HTMLTABLE}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
